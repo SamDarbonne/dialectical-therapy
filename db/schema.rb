798ac11dfcde77a0531ccc768b29807b38d21ad0
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-14 12:23:11 -0700

    user events association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014191200) do
+ActiveRecord::Schema.define(version: 20161014192228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20161014191200) do
     t.text     "victory"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema.define(version: 20161014191200) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "events", "users"
 end
