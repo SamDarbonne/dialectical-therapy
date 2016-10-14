3a2a5e1571498bca47d38ded91be890b5d7a025f
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-14 14:09:09 -0700

    create show even view and events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,14 @@
 class EventsController < ApplicationController
+  def index
+    @event = Event.all
+  end
+  def show
+    @event = Event.find_by_id(params[:id])
+    @user = @event.user
+  end
   def new
     @event = Event.new
+    user_id = params[:user_id]
+    @user = User.find_by(user_id: user_id)
   end
 end
