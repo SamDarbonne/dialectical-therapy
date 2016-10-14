28bd44477903f6bd12249a91bc9184265ce96350
14budig <nbudig@gmail.com>
2016-10-14 14:52:39 -0700

    adds validations and flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :events
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates :email, uniqueness:true
+  validates :user_name, uniqueness:true
 end
