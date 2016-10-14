3c42bbca0483ac5369ac28a41717420ed3a9179c
14budig <nbudig@gmail.com>
2016-10-14 12:36:20 -0700

    add root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "welcome#index"
+
   get "/login", to: "sessions#new"
   post "/sessions", to: "sessions#create"
   get "/logout", to: "sessions#destroy"
