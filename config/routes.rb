125470b8ac176fd899aa67902a821816851eead0
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-14 14:44:18 -0700

    basic form fields

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,6 @@ Rails.application.routes.draw do
 
   get '/events/:id', to: 'events#show', as: 'event'
   get '/users/:user_id/events/new', to: 'events#new', as: 'new_event'
-  post '/users/:user_id/events', to: 'events#create'
+  post '/users/:user_id/events', to: 'events#create', as: 'create_event'
 
 end
