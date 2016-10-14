d2ad549a2c3b14bf481d9aa11af0f55b61b75aff
14budig <nbudig@gmail.com>
2016-10-14 14:59:09 -0700

    fixes validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :events
-<<<<<<< HEAD
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :password, presence: true
-  validates :email, uniqueness:true
-  validates :user_name, uniqueness:true
-=======
+  validates :email, uniqueness: true, presence: true
+  validates :user_name, uniqueness: true, presence: true
+
   def self.confirm params
   	@user = User.find_by({user_name: params[:user_name]})
   	@user ? @user.authenticate(params[:password]) : false
   end
->>>>>>> 276cd27540703369927948a2208deb924279b1a3
 end
