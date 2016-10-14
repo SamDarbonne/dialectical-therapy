794dbc5339f7bd9c3ebee4be36159c1353e9f1a6
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-14 12:05:07 -0700

    users controller, seed, bycrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,41 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def new
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    redirect user_path
+  end
+
+  def show
+    @user = User.find_by_id(params [:id])
+  end
+
+  def edit
+    @user = User.find(params [:id])
+  end
+
+  def update
+    @user = User.find(params [:id])
+    if @user.update(user_params)
+      redirect user_path
+    end
   end
 
   def destroy
+    user_id = params[:id]
+    @user = User.find_by_id(user_id)
+    @user.destroy
+    redirect root_path
   end
 
-  def index
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :first_name, :last_name, :email, :password)
   end
 end
