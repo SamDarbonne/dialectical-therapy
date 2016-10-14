7de32a255b4746db553159e83a4b2f7b5f9ba492
14budig <nbudig@gmail.com>
2016-10-14 14:06:16 -0700

    post db:migrate with squashed migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014203940) do
+ActiveRecord::Schema.define(version: 20161014180943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,11 +44,6 @@ ActiveRecord::Schema.define(version: 20161014203940) do
     t.datetime "updated_at",   null: false
     t.integer  "user_id"
     t.index ["user_id"], name: "index_events_on_user_id", using: :btree
-  end
-
-  create_table "johnnies", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
