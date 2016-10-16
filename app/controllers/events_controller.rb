540119a4588bac6e285fb1cd59535e55eb449bdd
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-16 10:31:32 -0700

    branch handoff from laura to misha

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,4 +11,20 @@ class EventsController < ApplicationController
     user_id = params[:user_id]
     @user = User.find_by(id: user_id)
   end
+  def create
+    user_id = params[:user_id]
+    @user = User.find_by(id: user_id)
+    @event = Event.create(event_params)
+      if @event.save
+        redirect_to user_path
+      else
+        redirect_to new_event_path
+    end
+  end
+
+  private
+  def event_params
+    event_info = params.require(:event),permit(:behavior, :trigger, :before, :before_notes, :after, :after_notes, :distraction, :soothing, :improving, :helpful, :hurtful, :reflection, :victory)
+    event_params = event_info.merge({user_id: params[:user_id]})
+  end
 end
