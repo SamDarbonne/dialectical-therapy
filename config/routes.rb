1093ecceb2aa784344f9e164e10394d1edeb2eb5
samdarbonne <samdarbonne@gmail.com>
2016-10-16 09:47:20 -0700

    adds styling

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
-  get '/users/:id/edit', to: 'users#edit', as: 'edit_user'
+  get '/users/:user_name/edit', to: 'users#edit', as: 'edit_user'
   patch '/users/:id', to: 'users#update'
   delete '/users/:id', to: 'users#destroy'
 
