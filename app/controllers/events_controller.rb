af23bdc336d402084af32ddc8c9eea0b8c3c02c2
sfnewzgirl <YOUR_GITHUB_EMAIL_ADDRESS>
2016-10-17 13:12:17 -0700

    fixes pretty url for events, and events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,26 +6,27 @@ class EventsController < ApplicationController
 
   def new
     @event = Event.new
-    user_id = params[:user_id]
-    @user = User.find_by(id: user_id)
+    user_name = params[:user_name]
+    @user = User.find_by(user_name: user_name)
   end
 
   def create
     @event = Event.create(event_params)
-    redirect_to user_path(@event.user)
+    user_name = params[:user_name]
+    redirect_to user_path(user_name)
   end
 
   def show
     event_id = params[:id]
     @event = Event.find_by(id: event_id)
-    user_id = params[:user_id]
-    @user = User.find_by(id: user_id)
+    user_name = params[:user_name]
+    @user = User.find_by(user_name: user_name)
   end
 
   def edit
     @event = Event.find_by_id(params[:id])
-    user_id = params[:user_id]
-    @user = User.find_by(id: user_id)
+    user_name = params[:user_name]
+    @user = User.find_by(user_name: user_name)
   end
 
   def update
@@ -44,7 +45,7 @@ class EventsController < ApplicationController
   private
   def event_params
     event_info = params.require(:event).permit(:behavior, :trigger, :before, :before_notes, :after, :after_notes, :distraction, :soothing, :improving, :helpful, :hurtful, :reflection, :victory)
-    event_params = event_info.merge({user_id: (params[:user_id])})
+    event_params = event_info.merge({user_id: current_user.id})
   end
 
 
