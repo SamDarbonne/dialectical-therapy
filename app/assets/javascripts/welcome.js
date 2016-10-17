6441fd72fe5acdb50fbfed2c2994215506f0a0ac
14budig <nbudig@gmail.com>
2016-10-16 17:14:02 -0700

    fixes nav styling

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,3 +2,9 @@
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
 
+$(document).ready(function() {
+	$('#welcome-sign-in-button').on('click', function() {
+		$('#welcome-home-box').toggleClass('hidden');
+		$('#welcom-sign-in-box').toggleClass('hidden')
+	})
+})
