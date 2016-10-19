11fee5890b8ee4d69422e70d98d692b60a0bdcc2
samdarbonne <samdarbonne@gmail.com>
2016-10-19 15:29:39 -0700

    edits seed file order

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
 Event.destroy_all
+User.destroy_all
 
 users = User.create([
   {
